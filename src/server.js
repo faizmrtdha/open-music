c282bb797e4bd08bffb03d686a51583df683ee6d
Faiz Murtadha <faizmrtdha99@gmail.com>
2022-04-21 21:43:50 +0700

    Revise

---
+++ src/server.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/db/AlbumsService');
+const AlbumValidator = require('./validator/albums');
+const SongsService = require('./services/db/SongsService');
+const SongsValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      // membuat response baru dari response toolkit sesuai kebutuhan error handling
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return response.continue || response;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
---
+++ src/services/db/SongsService.js
@@ -0,0 +1,104 @@
+/* eslint-disable no-underscore-dangle */
+
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const { mapDBSongToModel } = require('../../utils');
+
+class SongsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong({ title, year, genre, performer, duration, albumId }) {
+    const id = `song-${nanoid(16)}`;
+    const createdAt = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $8) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId, createdAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSong({ title, performer }) {
+    if (title && performer) {
+      const query = {
+        text: 'SELECT songs.id, title, performer FROM songs WHERE title ILIKE $1 AND performer ILIKE $2',
+        values: [`%${title}%`, `%${performer}%`],
+      };
+      const result = await this._pool.query(query);
+      return result.rows;
+    } else if (performer) {
+      const query = {
+        text: 'SELECT songs.id, title, performer FROM songs WHERE performer ILIKE $1',
+        values: [`%${performer}%`],
+      };
+      const result = await this._pool.query(query);
+      return result.rows;
+    } else if (title) {
+      const query = {
+        text: 'SELECT songs.id, title, performer FROM songs WHERE title ILIKE $1',
+        values: [`%${title}%`],
+      };
+      const result = await this._pool.query(query);
+      return result.rows;
+    } else {
+      const result = await this._pool.query('SELECT songs.id, title, performer FROM songs');
+      return result.rows;
+    }
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBSongToModel)[0];
+  }
+
+  async editSongByIdHandler(id, { title, year, genre, performer, duration, albumId }) {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6, updated_at = $7 WHERE id = $8 RETURNING id',
+      values: [title, year, genre, performer, duration, albumId, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
+    }
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+  }
+}
+
+module.exports = SongsService;
